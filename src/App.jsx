b7fe715002cc877ada9a7c9deb4629fdb33bf8ed
Franco Gilli <francogilli10@gmail.com>
2023-12-24 11:49:09 -0300

    Square visibility improved

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,14 @@ import { TURNS } from "./constants";
 import { checkWinnerFrom, checkEndGame } from "./logic/board";
 import { WinnerModal } from "./components/WinnerModal";
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState(TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board');
+    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+  });
+  const [turn, setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem('turn');
+    return turnFromStorage ?? TURNS.X
+  });
   const [winner, setWinner] = useState(null);
 
 
@@ -15,6 +21,9 @@ function App() {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+
+    window.localStorage.removeItem('board');
+    window.localStorage.removeItem('turn');
   }
 
 
@@ -27,6 +36,8 @@ function App() {
 
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+    window.localStorage.setItem('board', JSON.stringify(newBoard));
+    window.localStorage.setItem('turn', newTurn);
     const newWinner = checkWinnerFrom(newBoard);
     if (newWinner) {
       confetti();
