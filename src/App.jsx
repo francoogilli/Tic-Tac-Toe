ececbe608f73d78252d34f7d429f3d95794bd4a2
Franco Gilli <francogilli10@gmail.com>
2023-12-24 09:53:48 -0300

    Added if there is a tie or winner

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,17 @@ function App() {
     }
     return null
   }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  }
+
+  const checkEndGame = (newBoard) => {
+
+    return newBoard.every((square) => square !== null);
+  }
   const updateBoard = (index) => {
     if(board[index] || winner) return
 
@@ -59,12 +70,15 @@ function App() {
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       setWinner(newWinner);
+    }else if (checkEndGame(newBoard)) {
+      setWinner(false);
     }
   };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reiniciar</button>
       <section className="game">
         {board.map((_, index) => (
           <Square key={index} index={index} updateBoard={updateBoard}>
@@ -77,6 +91,28 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+      {
+        winner != null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                  ? 'Empate'
+                  : 'Ganador: ' 
+                }
+              </h2>
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer className="">
+                <button onClick={resetGame}>Reiniciar</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   );
 }
